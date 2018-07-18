24b6d65a5beec32f3383a3ec2041d9addfb57218
Ian DesJardins <ian.d.desjardins@gmail.com>
2018-07-17 23:42:36 -0400

    get most recent block number that's been synced from tx store

--- src/app/providers/transaction-sync.service.ts
+++ src/app/providers/transaction-sync.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import { TransactionsService } from './transactions.service';
+import { TransactionsStorageService } from './transactions-storage.service';
+import { Transaction } from '../models/transaction';
 
 @Injectable()
 export class TransactionSyncService {
+  private addresses: string[];
 
-  constructor() { }
+  constructor(
+    private transactionsService: TransactionsService,
+    private transactionStorageService: TransactionsStorageService) {
+    this.addresses = [];
+  }
 
+  setAddresses(addresses: string[]): TransactionSyncService {
+    this.addresses = addresses;
+    return this;
+  }
+
+  startSync(): void {
+    // todo
+  }
+
+  stopSync(): void {
+    // todo
+  }
+
+  async getMostRecentBlockFromTransactions(): Promise<number> {
+    const allTransactions = await this.transactionStorageService.getAll() as Transaction[];
+    if (allTransactions.length > 0) {
+      const syncedBlockNumbers = allTransactions.map(x => x.blockNumber);
+      return Math.max(...syncedBlockNumbers);
+    }
+    return 0;
+  }
 }
